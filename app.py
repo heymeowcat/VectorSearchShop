0fd7f9b365964d7aff9ce61d1bf376eae49be7ad
Vidura Fernando <heyvidura@gmail.com>
2024-03-12 19:10:48 +0530

    feat: added description generation via gemini vision model for descriptions before adding products

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import os
+from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
 import sqlite3
-import base64
 from io import BytesIO
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+try:
+    load_dotenv()
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    model_vision = genai.GenerativeModel('gemini-pro-vision')
+except Exception as e:
+    st.error(f"Error configuring Gemini API: {e}")
+    st.stop()
 
 # Connect to SQLite database
 conn = sqlite3.connect("products.db")
@@ -59,9 +70,35 @@ def display_products(products):
 def add_product_form():
     with st.sidebar:
         st.subheader("Add Product")
-        name = st.text_input("Product Name")
-        description = st.text_area("Product Description")
         file = st.file_uploader("Upload Product Image", type=["jpg", "png", "jpeg"])
+        name = st.text_input("Product Name")
+
+
+        if "default" not in st.session_state:
+            st.session_state["default"] = ""
+        description = st.text_area(
+            "Product Description", value=st.session_state["default"]
+        )
+
+        if st.button("Generate Description"):
+            if file:
+                image = Image.open(file)
+                try:
+                    with st.spinner("Generating description..."):
+                        prompt_parts_vision = [
+                            "Generate a product description for the product.\n\Product's image:\n\n",
+                            image,
+                        ]
+                        response = model_vision.generate_content(prompt_parts_vision)
+                        generated_description = response.text
+                        description = generated_description  
+                        st.session_state["default"] = description
+                        st.experimental_rerun()
+                except Exception as e:
+                    st.error(f"Error generating description: {str(e)}")
+            else:
+                st.warning("Please upload an image to generate a description.")
+
         if st.button("Add Product"):
             if name and description and file:
                 image = Image.open(file)
@@ -73,7 +110,6 @@ def add_product_form():
 
 def main():
     st.title("Product Shop")
-
     with st.sidebar:
         search_term = st.text_input("Search Products")
 
